2a3884779b92922ffe1410962eb04d9b011d0fcb
codegym-cong <46494938+codegym-cong@users.noreply.github.com>
2020-03-07 18:51:28 +0700

    them repository post

--- src/main/java/com/example/springbootbackend/repository/post/PostRepository.java
+++ src/main/java/com/example/springbootbackend/repository/post/PostRepository.java
@@ -3,5 +3,5 @@ package com.example.springbootbackend.repository.post;
 import com.example.springbootbackend.models.Post;
 import com.example.springbootbackend.repository.Repository;
 
-public class PostRepository extends Repository<Post> {
+public interface PostRepository extends Repository<Post> {
 }
--- src/main/java/com/example/springbootbackend/security/services/post/PostServiceImpl.java
+++ src/main/java/com/example/springbootbackend/security/services/post/PostServiceImpl.java
@@ -1,4 +1,32 @@
 package com.example.springbootbackend.security.services.post;
 
-public class PostServiceImpl {
+import com.example.springbootbackend.models.Post;
+import com.example.springbootbackend.repository.post.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class PostServiceImpl implements PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    @Override
+    public void save(Post post) {
+        postRepository.save(post);
+    }
+
+    @Override
+    public void remove(Long id) {
+        postRepository.remove(id);
+    }
 }
